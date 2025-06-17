f9edefd795e3814b22d66fc94204643dd31998de
Your Name <you@example.com>
2025-06-16 22:11:48 -0500

    new update

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,13 +5,13 @@ export function Footer() {
     <footer className="w-full py-3 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
       <div className="grow text-left">
         Kris Yotam (
-        <A target="_blank" href="https://twitter.com/krisyotam">
+        <A href="https://twitter.com/krisyotam">
           @krisyotam
         </A>
         )
       </div>
       <div>
-        <A target="_blank" href="https://github.com/krisyotam/blog">
+        <A href="https://github.com/krisyotam/blog">
           Source
         </A>
       </div>
